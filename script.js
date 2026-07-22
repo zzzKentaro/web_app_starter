c91078ce973807278237852e56aa7375420369cb
zzzKentaro <fujii.kentaro.z.9@gmail.com>
2026-07-22 14:20:30 +0900

    Update script.js
    やっぱり全文書いておくね

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+  JavaScriptは「動き」を作るためのファイルです。
+  ボタンを押した時の処理などをここに書きます。
+*/
+
+// HTMLから、id="countButton" のボタンを探してくる
+const countButton = document.querySelector("#countButton");
+
+// HTMLから、id="countNumber" の表示場所を探してくる
+const countNumber = document.querySelector("#countNumber");
+
+// カウントの最初の値
+let count = 0;
+
+countButton.addEventListener("click", () => {
+  // countを1増やす
+  count = count + 1;
+
+  // 画面に表示されている数字を更新する
+  countNumber.textContent = count;
+});
